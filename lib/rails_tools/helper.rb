6eff423d8a6827c94d65b220f1435751d2c583c5
Nando Vieira <fnando.vieira@gmail.com>
2010-01-25 13:04:49 -0200

    Discard flash messages.

--- lib/rails_tools/helper.rb
+++ lib/rails_tools/helper.rb
@@ -5,6 +5,7 @@ module RailsTools
 
       flash.each do |name, message|
         html << content_tag(:p, message, :class => "message #{name}")
+        flash.discard(name)
       end
 
       html.respond_to?(:html_safe!) ? html.html_safe! : html
